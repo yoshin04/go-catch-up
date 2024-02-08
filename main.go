314109be35cbbab39ae642e88a7b096655ac014e
yoshin04 <taigaby0411@gmail.com>
2024-02-08 13:01:18 +0900

    module package basic catch up

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-basics/calculator"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	sl := []int{1, 2, 3}
-	if len(sl) > 2 {
-		fmt.Println("Slice is empty")
-	}
+	godotenv.Load()
+	fmt.Println(os.Getenv("GO_ENV"))
+	fmt.Println(calculator.Offset)
+	fmt.Println(calculator.Sum(1, 2))
+	fmt.Println(calculator.Multiply(1, 2))
 }
