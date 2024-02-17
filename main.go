af7c4e4ccfa7fc0195022079b841bf2f778e53f4
yoshin04 <taigaby0411@gmail.com>
2024-02-18 00:46:28 +0900

    genericsキャッチアップ

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
+type customConstraints interface {
+	~int | int16 | float32 | float64 | string
+}
+
+type NewInt int
+
+func add[T customConstraints](x, y T) T {
+	return x + y
+}
+
 func main() {
-	err01 := errors.New("something wrong")
-	fmt.Printf("%[1]p %[1]T %[1]v\n", err01)
+	fmt.Printf("%v\n", add(1, 2))
+	fmt.Printf("%v\n", add(1.1, 2.2))
+	fmt.Printf("%v\n", add("Hello, ", "World!"))
+	var i1, i2 NewInt = 3, 4
+	fmt.Printf("%v\n", add(i1, i2))
 }
