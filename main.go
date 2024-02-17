4716b39fffdd311bd2c0b3c29145644110dc9b1b
yoshin04 <taigaby0411@gmail.com>
2024-02-18 00:33:06 +0900

    erros catch up

--- main.go
+++ main.go
@@ -1,48 +1,11 @@
 package main
 
-import "fmt"
-
-type controller interface {
-	speedUp() int
-	speedDown() int
-}
-
-type vehicle struct {
-	speed       int
-	enginePower int
-}
-
-type bycycle struct {
-	speed      int
-	humanPower int
-}
-
-func (v *vehicle) speedUp() int {
-	v.speed += 10 * v.enginePower
-	return v.speed
-}
-
-func (v *vehicle) speedDown() int {
-	v.speed -= 5 * v.enginePower
-	return v.speed
-}
-
-func (v *bycycle) speedUp() int {
-	v.speed += 3 * v.humanPower
-	return v.speed
-}
-func (v *bycycle) speedDown() int {
-	v.speed -= 1 * v.humanPower
-	return v.speed
-}
-func speedUpAndDown(c controller) {
-	fmt.Printf("current speed: %v\n", c.speedUp())
-	fmt.Printf("current speed: %v\n", c.speedDown())
-}
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	v := &vehicle{0, 5}
-	speedUpAndDown(v)
-	b := &bycycle{0, 5}
-	speedUpAndDown(b)
+	err01 := errors.New("something wrong")
+	fmt.Printf("%[1]p %[1]T %[1]v\n", err01)
 }
