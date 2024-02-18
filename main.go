1479f5bb104212e766d69f54408b39dcc971ab0a
yoshin04 <taigaby0411@gmail.com>
2024-02-18 11:38:59 +0900

    goroutine catch up

--- main.go
+++ main.go
@@ -1,23 +1,78 @@
 package main
 
 import (
-	"io"
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"runtime/trace"
+	"sync"
+	"time"
 )
 
 func main() {
-	file, err := os.Create("log.txt")
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	fmt.Println("goroutine invoked")
+	// }()
+	// wg.Wait()
+	// fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
+	// fmt.Println("main function invoked")
+
+	f, err := os.Create("trace.out")
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Error:", err)
 	}
 
-	defer file.Close()
-	flags := log.Lshortfile
-	warnLogger := log.New(io.MultiWriter(file, os.Stderr), "WARNING: ", flags)
-	errorLogger := log.New(io.MultiWriter(file, os.Stderr), "ERROR: ", flags)
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalln("Error:", err)
+		}
+	}()
+	if err := trace.Start(f); err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer trace.Stop()
+	ctx, t := trace.NewTask(context.Background(), "main")
+	defer t.End()
+	fmt.Println("The number of logical CPU Cores:", runtime.NumCPU())
 
-	warnLogger.Println("warning a")
-	// ログを出力した後に強制終了
-	errorLogger.Fatalln("critical error")
+	// task(ctx, "Task1")
+	// task(ctx, "Task2")
+	// task(ctx, "Task3")
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go cTask(ctx, &wg, "Task1")
+	go cTask(ctx, &wg, "Task2")
+	go cTask(ctx, &wg, "Task3")
+	wg.Wait()
+	fmt.Println("main func finish")
+
+	s := []int{1, 2, 3}
+	for _, i := range s {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Println()
+		}(i)
+		wg.Wait()
+
+		fmt.Println("main func finish")
+	}
+}
+
+func task(ctx context.Context, name string) {
+	defer trace.StartRegion(ctx, name).End()
+	time.Sleep(2 * time.Second)
+	fmt.Println(name)
+}
+
+func cTask(ctx context.Context, wg *sync.WaitGroup, name string) {
+	defer trace.StartRegion(ctx, name).End()
+	defer wg.Done()
+	time.Sleep(time.Second)
+	fmt.Println(name)
 }
