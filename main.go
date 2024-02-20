1425c2cead96713bd827ff3c668cb2754deb0ee8
yoshin04 <taigaby0411@gmail.com>
2024-02-20 18:42:20 +0900

    channnel catchup

--- main.go
+++ main.go
@@ -2,28 +2,64 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sync"
+	"time"
 )
 
 func main() {
-	// ch := make(chan int, 1)
-	// var wg sync.WaitGroup
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	ch <- 10
-	// 	time.Sleep(300 * time.Millisecond)
-	// }()
-	// fmt.Println(<-ch)
-	// wg.Wait()
 	ch1 := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		fmt.Println(<-ch1)
 	}()
-	ch1 <- 10
-	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
+	close(ch1)
+	v, ok := <-ch1
+	fmt.Printf("%v %v\n", v, ok)
+	wg.Wait()
 
-	ch2 := make(chan int, 1)
+	ch2 := make(chan int, 2)
+	ch2 <- 1
 	ch2 <- 2
-	fmt.Println(<-ch2)
+	close(ch2)
+	v, ok = <-ch2
+	fmt.Printf("%v %v\n", v, ok)
+	v, ok = <-ch2
+	fmt.Printf("%v %v\n", v, ok)
+	v, ok = <-ch2
+	fmt.Printf("%v %v\n", v, ok)
+
+	ch3 := generateCountStream()
+	for v := range ch3 {
+		fmt.Println(v)
+	}
+
+	nCh := make(chan struct{})
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Printf(("goroutine %v started\n"), i)
+			<-nCh
+			fmt.Println(i)
+		}(i)
+	}
+	time.Sleep(2 * time.Second)
+	close(nCh)
+	fmt.Println("unblocked by manual close")
+
+	wg.Wait()
+	fmt.Println("finish")
+}
+
+func generateCountStream() <-chan int {
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+	return ch
 }
