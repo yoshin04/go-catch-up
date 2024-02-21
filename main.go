e36d32b652e1cd9b21a9894bd807d7cb8f7ff600
yoshin04 <taigaby0411@gmail.com>
2024-02-21 17:08:42 +0900

    catch up select

--- main.go
+++ main.go
@@ -1,65 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 func main() {
-	ch1 := make(chan int)
+	ch1 := make(chan string, 1)
+	ch2 := make(chan string, 1)
 	var wg sync.WaitGroup
-	wg.Add(1)
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Millisecond)
+	defer cancel()
+	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		fmt.Println(<-ch1)
+		time.Sleep(500 * time.Millisecond)
+		ch1 <- "A"
 	}()
-	close(ch1)
-	v, ok := <-ch1
-	fmt.Printf("%v %v\n", v, ok)
-	wg.Wait()
-
-	ch2 := make(chan int, 2)
-	ch2 <- 1
-	ch2 <- 2
-	close(ch2)
-	v, ok = <-ch2
-	fmt.Printf("%v %v\n", v, ok)
-	v, ok = <-ch2
-	fmt.Printf("%v %v\n", v, ok)
-	v, ok = <-ch2
-	fmt.Printf("%v %v\n", v, ok)
-
-	ch3 := generateCountStream()
-	for v := range ch3 {
-		fmt.Println(v)
+	go func() {
+		defer wg.Done()
+		time.Sleep(500 * time.Millisecond)
+		ch2 <- "B"
+	}()
+loop:
+	for ch1 != nil || ch2 != nil {
+		select {
+		case <-ctx.Done():
+			fmt.Println("timeout")
+			break loop
+		case v := <-ch1:
+			fmt.Println(v)
+			ch1 = nil
+		case v := <-ch2:
+			fmt.Println(v)
+			ch2 = nil
+		}
 	}
-
-	nCh := make(chan struct{})
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			fmt.Printf(("goroutine %v started\n"), i)
-			<-nCh
-			fmt.Println(i)
-		}(i)
-	}
-	time.Sleep(2 * time.Second)
-	close(nCh)
-	fmt.Println("unblocked by manual close")
-
 	wg.Wait()
 	fmt.Println("finish")
-}
-
-func generateCountStream() <-chan int {
-	ch := make(chan int)
-	go func() {
-		defer close(ch)
-		for i := 0; i < 10; i++ {
-			ch <- i
-		}
-	}()
-	return ch
 }
