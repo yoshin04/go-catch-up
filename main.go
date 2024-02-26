17108e2961ebe1a7b83e958c4b0da37284a2042f
yoshin04 <taigaby0411@gmail.com>
2024-02-26 20:33:24 +0900

    catch up select

--- main.go
+++ main.go
@@ -1,43 +1,31 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
+const bufSize = 3
+
 func main() {
-	ch1 := make(chan string, 1)
-	ch2 := make(chan string, 1)
 	var wg sync.WaitGroup
-	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Millisecond)
-	defer cancel()
-	wg.Add(2)
+	ch := make(chan string, bufSize)
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		time.Sleep(500 * time.Millisecond)
-		ch1 <- "A"
-	}()
-	go func() {
-		defer wg.Done()
-		time.Sleep(500 * time.Millisecond)
-		ch2 <- "B"
-	}()
-loop:
-	for ch1 != nil || ch2 != nil {
-		select {
-		case <-ctx.Done():
-			fmt.Println("timeout")
-			break loop
-		case v := <-ch1:
-			fmt.Println(v)
-			ch1 = nil
-		case v := <-ch2:
-			fmt.Println(v)
-			ch2 = nil
+		for i := 0; i < 5; i++ {
+			time.Sleep(1000 * time.Millisecond)
+			ch <- "hello"
 		}
+	}()
+	for i := 0; i < 3; i++ {
+		select {
+		case m := <-ch:
+			fmt.Println(m)
+		default:
+			fmt.Println("no msg arrived")
+		}
+		time.Sleep(1500 * time.Millisecond)
 	}
-	wg.Wait()
-	fmt.Println("finish")
 }
